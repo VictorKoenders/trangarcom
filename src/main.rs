72319894537ce4ec133b3ffe1d7b2389a767b9ee
Trangar <victor.koenders@gmail.com>
2022-01-16 09:52:29 +0100

    Switched from tokio+actix to async_std+tide (#79)
    * Switched from tokio+actix to async_std+tide
    
    * Removed commented out code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,26 @@
-use actix_web::{App, HttpServer};
 use std::sync::Arc;
 
 mod data;
 mod middleware;
 mod routes;
 
-#[tokio::main]
+#[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
-    let _ = dotenv::dotenv(); // this may fail, we don't always have a .env file
-
-    let local = tokio::task::LocalSet::new();
-    let sys = actix_rt::System::run_in_tokio("server", &local);
-
-    let context = data::DbContext::default();
-    let registry = Arc::new(prometheus::Registry::new());
-    let prometheus = middleware::Prometheus::new(&registry);
+    let context = Context::default();
+    let registry = Arc::new(context.prometheus.clone());
 
     let addr = "0.0.0.0:8000";
-    HttpServer::new(move || {
-        let registry = Arc::clone(&registry);
-        let prometheus = prometheus.clone();
-        App::new()
-            .data(context.clone())
-            .app_data(registry)
-            .wrap(actix_web::middleware::Compress::default())
-            .wrap(prometheus)
-            .configure(routes::configure)
-    })
-    .bind(addr)?
-    .run()
-    .await?;
 
-    sys.await?;
+    let mut server = tide::with_state(context);
 
-    Ok(())
+    server.with(middleware::Prometheus::new(&registry));
+
+    routes::configure(&mut server);
+    server.listen(addr).await
+}
+
+#[derive(Default, Clone)]
+pub struct Context {
+    pub data: data::DbContext,
+    pub prometheus: prometheus::Registry,
 }

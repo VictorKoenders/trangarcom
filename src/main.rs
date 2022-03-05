cb841dc7d94cd80135dfb2ecee957cb00d4cd7d7
Victor Koenders <gpg@trangar.com>
2022-03-05 10:03:46 +0100

    Added args to switch the port

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::Arc;
+use clap::Parser;
+use std::{net::SocketAddr, sync::Arc};
 
 mod data;
 mod middleware;
@@ -6,21 +7,27 @@ mod routes;
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
     let context = Context::default();
     let registry = Arc::new(context.prometheus.clone());
-
-    let addr = "0.0.0.0:8000";
 
     let mut server = tide::with_state(context);
 
     server.with(middleware::Prometheus::new(&registry));
 
     routes::configure(&mut server);
-    server.listen(addr).await
+    server.listen(args.addr).await
 }
 
 #[derive(Default, Clone)]
 pub struct Context {
     pub data: data::DbContext,
     pub prometheus: prometheus::Registry,
+}
+
+#[derive(Parser)]
+pub struct Args {
+    /// Which address to listen on
+    #[clap(short, long, default_value = "127.0.0.1:8000")]
+    pub addr: SocketAddr,
 }
